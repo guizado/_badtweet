a387db31e6e7799d8f6333448aa8127babdde240
guizado <61948623+guizado@users.noreply.github.com>
2020-06-26 14:58:30 +0100

    Delete frame_cred.py

--- badtweet/frame_cred.py
+++
@@ -1,5 +0,0 @@
-consumer_key = "BUXWMswjrz2SHTDJ8qZyPC4Dh"
-consumer_secret = "n7CV18J3ylpP1pxLsY0S5AHR8HKR4abpJz1gayH3VNy877XgTC"
-token = "1249296868548521986-gPaeExFKR1WiBe0cUIb0uRxJrM3T1C"
-token_secret = "EUlgDUYRivVztOxQLGRNstFy90En4I54eyGQYpICJZhR6"
-
